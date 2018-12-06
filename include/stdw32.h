ad6e2c469de0c95d3a84ffd0ebc8ac8fd7ac6f66
frank2 <frank2@dc949.org>
2018-12-05 18:35:54 -0800

    sketch out initial headers

---
+++ include/stdw32.h
@@ -0,0 +1,18 @@
+#ifndef __STDW32_H
+#define __STDW32_H
+
+#include <stdw32/buffer.h>
+#include <stdw32/char.h>
+#include <stdw32/console.h>
+#include <stdw32/dir.h>
+#include <stdw32/file.h>
+#include <stdw32/io.h>
+#include <stdw32/memory.h>
+#include <stdw32/output.h>
+#include <stdw32/process.h>
+#include <stdw32/runtime.h>
+#include <stdw32/stream.h>
+#include <stdw32/string.h>
+#include <stdw32/time.h>
+
+#endif
