1a95acd9f3208f94d8c3393458d6d8e76edb5cbe
frank2 <frank2@dc949.org>
2018-12-10 01:28:31 -0800

    forgot string.c

---
+++ lib/string.c
@@ -0,0 +1,186 @@
+#include <stdw32/string.h>
+#include <windows.h>
+
+char *
+strcat
+(char *destination, const char *src)
+{
+   if (destination == NULL || src == NULL)
+      return NULL;
+
+   return lstrcatA(destination, src);
+}
+
+wchar_t *
+wcscat
+(wchar_t *destination, const wchar_t *source)
+{
+   if (destination == NULL || src == NULL)
+      return NULL;
+
+   return lstrcatW(destination, src);
+}
+
+#ifdef __cplusplus
+const char *
+strchr
+(const char *str, int character)
+{
+   size_t index, size;
+
+   if (str == NULL)
+      return NULL;
+
+   size = strlen(str);
+
+   for (index=0; index<size; ++index)
+      if (str[index] == (char)character)
+         return &str[index];
+
+   return NULL;
+}
+
+const wchar_t *
+wcschr
+(const wchar_t *str, wchar_t character)
+{
+   size_t index, size;
+
+   if (str == NULL)
+      return NULL;
+
+   size = wcslen(str);
+
+   for (index=0; index<size; ++index)
+      if (str[index] == (wchar_t)character)
+         return &str[index];
+
+   return NULL;
+}
+#endif
+
+char *
+strchr
+(char *str, int character)
+{
+   size_t index, size;
+
+   if (str == NULL)
+      return NULL;
+
+   size = strlen(str);
+
+   for (index=0; index<size; ++index)
+      if (str[index] == (char)character)
+         return &str[index];
+
+   return NULL;
+}
+
+wchar_t *
+wcschr
+(wchar_t *str, wchar_t character)
+{
+   size_t index, size;
+
+   if (str == NULL)
+      return NULL;
+
+   size = wcslen(str);
+
+   for (index=0; index<size; ++index)
+      if (str[index] == (wchar_t)character)
+         return &str[index];
+
+   return NULL;
+}
+
+int
+strcmp
+(const char *str1, const char *str2)
+{
+   return lstrcmpA(str1, str2);
+}
+
+int
+wcscmp
+(const wchar_t *str1, const wchar_t *str2)
+{
+   return lstrcmpW(str1, str2);
+}
+
+int
+strcpy
+(const char *str1, const char *str2)
+{
+   return lstrcpyA(str1, str2);
+}
+
+int
+wcscpy
+(const wchar_t *str1, const wchar_t *str2)
+{
+   return lstrcpyW(str1, str2);
+}
+
+size_t
+strcspn
+(const char *search, const char *set)
+{
+   const char *index;
+   
+   if (search == NULL || set == NULL)
+      return 0;
+
+   index = search;
+
+   while (*index != 0)
+   {
+      const char *scan = set;
+
+      while (*scan != 0)
+         if (*index == *scan)
+            goto characterFound;
+   }
+
+characterFound:
+   return ( (size_t)(index - search) );
+}
+
+size_t
+wcscspn
+(const wchar_t *search, const wchar_t *set)
+{
+   const wchar_t *index;
+   
+   if (search == NULL || set == NULL)
+      return 0;
+
+   index = search;
+
+   while (*index != 0)
+   {
+      const wchar_t *scan = set;
+
+      while (*scan != 0)
+         if (*index == *scan)
+            goto characterFound;
+   }
+
+characterFound:
+   return ( (size_t)(index - search) );
+}
+
+size_t
+strlen
+(const char *str)
+{
+   return lstrlenA(str);
+}
+
+size_t
+wcslen
+(const wchar_t *str)
+{
+   return lstrlenW(str);
+}
